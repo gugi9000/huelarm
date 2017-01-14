5f0255e249dc8b1dc8864f493579a5d3cc87be1d
Bjarke Sørensen <bs@wasd.dk>
2017-01-14 23:45:13 +0100

    Clean up php code

--- service.php
+++ service.php
@@ -1,35 +1,25 @@
 <?php
 $overskredet = 0;
 include ("connect.db.php");
-
 $query = mssql_query('
 SELECT [Received]
       ,[TimeoutNotificationSent]
   FROM [RIS].[dbo].[MailCase] 
-  
   where
   [HandledByEmployeeId] IS NULL and
   [MailCaseMailboxId] = 10011
 ORDER BY [Received] DESC
 ');
-
-$j=0;
-
 do {
 	while ($row = mssql_fetch_row($query)) {
 	    if (count($row) > 4) { $overskredet=1;}
 	    if ($row[1]==1) {$overskredet = 2;} 
-	    $j++;
 	}
 } while (mssql_next_result($query));
-
-
 mssql_free_result($query);
-
 if ($overskredet == 1) {
         print 1;
 } else if ($overskredet == 2) {
         print 2;
 }
-
 ?>
