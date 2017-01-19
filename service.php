cb63bca77a268353e16af49c0f5a30dd3e54b377
Bjarke Sørensen <bs@wasd.dk>
2017-01-19 22:38:48 +0100

    Update service to give correct responses
    Even if there are no issues

--- service.php
+++ service.php
@@ -1,25 +1,37 @@
 <?php
 $overskredet = 0;
 include ("connect.db.php");
+
 $query = mssql_query('
 SELECT [Received]
       ,[TimeoutNotificationSent]
-  FROM [RIS].[dbo].[MailCase] 
+  FROM [RIS].[dbo].[MailCase]
+
   where
   [HandledByEmployeeId] IS NULL and
   [MailCaseMailboxId] = 10011
 ORDER BY [Received] DESC
 ');
+
+$j=0;
+
 do {
-	while ($row = mssql_fetch_row($query)) {
-	    if (count($row) > 4) { $overskredet=1;}
-	    if ($row[1]==1) {$overskredet = 2;} 
-	}
+        while ($row = mssql_fetch_row($query)) {
+            if ($row[1]==0) { $overskredet=1;}
+            if ($row[1]==1) {$overskredet = 2;}
+            $j++;
+        }
 } while (mssql_next_result($query));
+
+
 mssql_free_result($query);
+
 if ($overskredet == 1) {
         print 1;
 } else if ($overskredet == 2) {
         print 2;
+} else  if ($overskredet == 0){
+       print 0;
 }
+
 ?>
